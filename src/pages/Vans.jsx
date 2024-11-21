45f05379f39c53373287887ff91afeba065a2641
Sheng232 <Shengj12345@gmail.com>
2024-11-20 20:27:33 -0800

    added the filter css

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -3,7 +3,24 @@ import Van from "../components/Van"
 import {Link} from "react-router-dom"
 export default function Vans(){
     const [vanState, setVanState] = useState([]);
-
+    // const [filterState, setFilterState] = useState(
+    //     [
+    //     {
+    //         name: 'Simple',
+    //         isClick: false,
+    //         color: '#E17654',
+    //     }, 
+    //     {
+    //         name: 'Luxury',
+    //         isClick: false,
+    //         color: '#115E59',
+    //     }, 
+    //     {
+    //         name: 'Rugged',
+    //         isClick: false,
+    //         color: '#161616',
+            
+    //     }]);
 
     useEffect(
         ()=>{
@@ -19,6 +36,31 @@ export default function Vans(){
         } 
         fetchData();
 },[]);
+
+    // function updateFilter(event){
+    //     setFilterState(prevState=>{
+    //         return(
+    //             prevState.map(button=>{
+    //                 const newIsClick = !button.isClick;
+    //                 if(event.target.innerText === button.name){
+    //                 return(
+                        
+    //                     {
+    //                         ...button,
+    //                         isClick: !button.isClick,
+    //                         color: newIsClick ? '#FFEAD0' : button.color === '#FFEAD0' ? button.originalColor : button.color,
+    //                         originalColor: button.originalColor || button.color, // Preserve the original color
+    //                     }
+    //                 )}
+    //                 else{
+    //                     return button
+    //                 }
+
+    //             })
+    //         )
+    //     })
+    // }
+    // console.log(filterState)
 
 const allVans = vanState.map(van=>{
     const {id, name, price, description, imageUrl, type} = van
@@ -40,7 +82,12 @@ const allVans = vanState.map(van=>{
     return(
         <div className="margin-1rem">
             <h1 className = "title">Explore our van options</h1>
-            <div className = "van-filter">{/*the filter goes here */}</div>
+            <div className = "van-filter">
+                <button>Simple</button>
+                <button>Luxury</button>
+                <button>Rugged</button>
+                <button className="filter-button"><span className="black-underline">Clear filters</span></button>
+            </div>
             <div className = "flex-row">
                 {allVans}
             </div>
